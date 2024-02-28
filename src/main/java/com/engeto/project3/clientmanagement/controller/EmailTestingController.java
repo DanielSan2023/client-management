591d225a95a7853d4a8b4883b3ed8300837fbf14
DanielSan2023 <electronic.san@gmail.com>
2024-02-28 19:27:30 +0100

    Revert "added email.send.enable value for permission email sending"
    This reverts commit 5d903cbb82608b11b2ae9a63093a15721937f07c.

--- src/main/java/com/engeto/project3/clientmanagement/controller/EmailTestingController.java
+++ src/main/java/com/engeto/project3/clientmanagement/controller/EmailTestingController.java
@@ -1,33 +1,20 @@
 package com.engeto.project3.clientmanagement.controller;
 
 import com.engeto.project3.clientmanagement.config.email.SendEmailStep;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@AllArgsConstructor
 @RestController
-@Slf4j
 @RequestMapping("/send-test-email")
 public class EmailTestingController {
 
     private final SendEmailStep sendEmailStep;
 
-    public EmailTestingController(SendEmailStep sendEmailStep) {
-        this.sendEmailStep = sendEmailStep;
-    }
-
-    @Value("${email.send.enable}")
-    private boolean sendEmail;
-
     @RequestMapping
-    public void sentEmailTest() {
-        if (sendEmail) {
-            log.error("Send email is not supported on production. ");
-        } else {
-            sendEmailStep.sendEmailToClient();
-            log.error("Sending an email. ");
-        }
+    public String sentEmailTest() {
+        sendEmailStep.sendEmailToClient();
+        return null;
     }
 }
